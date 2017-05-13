c5a4a081027115b2fdea2d9a9a1808394924f0e3
Michael <Michael@DESKTOP-PV8N89Q>
2017-05-13 17:16:54 +0300

    תיקונים צבע והוספת קונטרולר

---
+++ AlexMichaelProject/Controllers/productsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AlexMichaelProject.Models;
+
+namespace AlexMichaelProject.Controllers
+{
+    public class productsController : Controller
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: products
+        public async Task<ActionResult> Index()
+        {
+            var products = db.products.Include(p => p.manufacturer1).Include(p => p.team);
+            return View(await products.ToListAsync());
+        }
+
+        // GET: products/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = await db.products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: products/Create
+        public ActionResult Create()
+        {
+            ViewBag.manufacturer = new SelectList(db.manufacturers, "mname", "msite");
+            ViewBag.clubName = new SelectList(db.teams, "teamName", "leuge");
+            return View();
+        }
+
+        // POST: products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "product1,type,manufacturer,clubName,cost,size,description,photo")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.products.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.manufacturer = new SelectList(db.manufacturers, "mname", "msite", product.manufacturer);
+            ViewBag.clubName = new SelectList(db.teams, "teamName", "leuge", product.clubName);
+            return View(product);
+        }
+
+        // GET: products/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = await db.products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.manufacturer = new SelectList(db.manufacturers, "mname", "msite", product.manufacturer);
+            ViewBag.clubName = new SelectList(db.teams, "teamName", "leuge", product.clubName);
+            return View(product);
+        }
+
+        // POST: products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "product1,type,manufacturer,clubName,cost,size,description,photo")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.manufacturer = new SelectList(db.manufacturers, "mname", "msite", product.manufacturer);
+            ViewBag.clubName = new SelectList(db.teams, "teamName", "leuge", product.clubName);
+            return View(product);
+        }
+
+        // GET: products/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = await db.products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            product product = await db.products.FindAsync(id);
+            db.products.Remove(product);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
