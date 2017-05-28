0640c38963641d8b774a219e495114ac9570b4ec
AlexKagan107 <alex.kagan107@gmail.com>
2017-05-28 23:20:36 +0300

    manufacturers

--- AlexMichaelProject/Controllers/manufacturersController.cs
+++ AlexMichaelProject/Controllers/manufacturersController.cs
@@ -48,14 +48,25 @@ namespace AlexMichaelProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "mname,msite")] manufacturer manufacturer)
         {
+            string result = "";
             if (ModelState.IsValid)
             {
-                db.manufacturers.Add(manufacturer);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+                manufacturer dbMan = await db.manufacturers.FindAsync(manufacturer.mname);
+                if(dbMan == null)
+                {
+                    db.manufacturers.Add(manufacturer);
+                    await db.SaveChangesAsync();
+                    //return RedirectToAction("Index");
+                    result = "true";
+                }
+                else
+                {
+                    result = "false";
+                }
 
-            return View(manufacturer);
+            }
+            //return View(manufacturer);
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public async Task<ActionResult> Edit(string id)
@@ -76,13 +87,16 @@ namespace AlexMichaelProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "mname,msite")] manufacturer manufacturer)
         {
+            string result = "";
             if (ModelState.IsValid)
             {
                 db.Entry(manufacturer).State = EntityState.Modified;
                 await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                //return RedirectToAction("Index");
+                result = "true";
             }
-            return View(manufacturer);
+            //return View(manufacturer);
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public async Task<ActionResult> Delete(string id)
@@ -99,14 +113,25 @@ namespace AlexMichaelProject.Controllers
             return View(manufacturer);
         }
 
-        [HttpPost, ActionName("Delete")]
+        //[HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(string id)
         {
-            manufacturer manufacturer = await db.manufacturers.FindAsync(id);
-            db.manufacturers.Remove(manufacturer);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            string result = "";
+            try
+            {
+                manufacturer manufacturer = await db.manufacturers.FindAsync(id);
+                db.manufacturers.Remove(manufacturer);
+                await db.SaveChangesAsync();
+                //return RedirectToAction("Index");
+                result = "true";
+            }
+            catch(Exception ex)
+            {
+                result = "false";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
